fa629e8b5efa1f958beb5496c4111c83632591d5
dalabi91 <152018805+dalabi91@users.noreply.github.com>
2024-01-22 15:17:31 +0000

    added timeblock and code to display current date in header

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+// Function to initialize the scheduler
+// function initScheduler() {
+// Get the current date and display it in header
+var currentDate = dayjs();
+$("#currentDay").text(currentDate.format("dddd, MMMM D, YYYY"));
