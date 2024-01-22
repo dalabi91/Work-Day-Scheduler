f79a4a250440abeaae5cd5b2ea1747279a7c2616
dalabi91 <152018805+dalabi91@users.noreply.github.com>
2024-01-22 16:02:37 +0000

    pseudo code added

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,15 @@
 // Get the current date and display it in header
 var currentDate = dayjs();
 $("#currentDay").text(currentDate.format("dddd, MMMM D, YYYY"));
+
+// Loop through the hours of the day to create timeblocks
+
+// Add past, present, or future class based on the current time
+
+// get saved events from local storage
+
+// on button click save event to local storage
+
+// Append the columns to the timeblock and the timeblock to the container
+
+//call function
