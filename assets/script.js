4e3cf753096f217b73c964edadc423217e8f64f0
dalabi91 <152018805+dalabi91@users.noreply.github.com>
2024-01-22 22:24:25 +0000

    ordinal suffix now working, plugin added

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
 // Function to initialize the scheduler
 // function initScheduler() {
+
 // Get the current date and display it in header
 var currentDate = dayjs();
-$("#currentDay").text(currentDate.format("dddd, MMMM D, YYYY"));
+$("#currentDay").text(currentDate.format("dddd, MMMM Do"));
 
 $(document).ready(function () {
   // Function to update timeblock styling based on the current time
@@ -26,6 +27,7 @@ $(document).ready(function () {
   // Function to save events to local storage on button click
   $(".saveBtn").on("click", function () {
     var eventText = $(this).siblings(".description").val();
+
     var eventHour = parseInt($(this).parent().attr("id").replace("hour", ""));
     localStorage.setItem("event_" + eventHour, eventText);
   });
